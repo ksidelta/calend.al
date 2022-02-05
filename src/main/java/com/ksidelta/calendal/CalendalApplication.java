0f9ce3e63d4a7bdeff3f37a0aa8240c8196512e8
psuwala <oneat@vp.pl>
2022-02-05 19:07:05 +0100

    Created Calend.al

---
+++ src/main/java/com/ksidelta/calendal/CalendalApplication.java
@@ -0,0 +1,13 @@
+package com.ksidelta.calendal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendalApplication.class, args);
+	}
+
+}
