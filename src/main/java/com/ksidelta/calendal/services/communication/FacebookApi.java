04e43da03cbb5658aac0561a1836ac7bddf8e530
psuwala <oneat@vp.pl>
2022-02-16 14:29:01 +0100

    created more stuff for facebook

--- src/main/java/com/ksidelta/calendal/services/communication/FacebookApi.java
+++ src/main/java/com/ksidelta/calendal/services/communication/FacebookApi.java
@@ -14,30 +14,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-@Import(FacebookApi.FacebookClientConfiguration.class)
 public class FacebookApi {
     @Autowired
-    FacebookClient facebookClient;
+    protected FacebookConfig facebookConfig;
 
-    public List<Page> fetchPages(String fuzzyPageName){
-        var conn = facebookClient.fetchConnection("pages", Page.class, Parameter.with("q", fuzzyPageName));
-        return conn.getData();
+    @Autowired
+    protected RestTemplate restTemplate;
+
+    public List<Page> fetchPages(String fuzzyPageName) {
+        var str = restTemplate.getForObject(
+                String.format("https://graph.facebook.com/pages/search?q=%s&fields=id,name,location,link&access_token=%s", fuzzyPageName, facebookConfig.getAccessToken()),
+                String.class
+        );
+
+        return null;
     }
 
     @Configuration
-    static class FacebookClientConfiguration {
-        @Autowired
-        FacebookConfig facebookConfig;
-
+    static class InternalConfig {
         @Bean
-        public FacebookClient facebookClient() {
-            return new DefaultFacebookClient(
-                    facebookConfig.clientId,
-                    facebookConfig.clientSecret,
-                    Version.VERSION_12_0);
+        public RestTemplate restTemplate() {
+            return new RestTemplate();
         }
     }
+
 
 }
---
+++ src/main/java/com/ksidelta/calendal/services/events/FacebookEventService.java
@@ -0,0 +1,4 @@
+package com.ksidelta.calendal.services.events;
+
+public class FacebookEventService {
+}
