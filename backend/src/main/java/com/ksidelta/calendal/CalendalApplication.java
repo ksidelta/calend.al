700f1970e82430af952ff0e034ce56c9319075cf
psuwala <oneat@vp.pl>
2022-02-24 20:02:03 +0100

    added random mock endpoint + disabled security

--- backend/src/main/java/com/ksidelta/calendal/CalendalApplication.java
+++ backend/src/main/java/com/ksidelta/calendal/CalendalApplication.java
@@ -1,13 +1,17 @@
 package com.ksidelta.calendal;
 
+import com.ksidelta.calendal.mock.MockController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@Import(MockController.class)
 public class CalendalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CalendalApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CalendalApplication.class, args);
+    }
 
 }
--- backend/src/main/java/com/ksidelta/calendal/config/FacebookConfig.java
+++ backend/src/main/java/com/ksidelta/calendal/config/FacebookConfig.java
@@ -1,17 +1,19 @@
 package com.ksidelta.calendal.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.stereotype.Component;
 
 @Component
+
 public class FacebookConfig {
     @Value("${facebook.clientId}")
     public String clientId;
-    
+
     @Value("${facebook.clientSecret}")
     public String clientSecret;
 
-    public String getAccessToken(){
+    public String getAccessToken() {
         return this.clientId + "|" + this.clientSecret;
     }
 }
